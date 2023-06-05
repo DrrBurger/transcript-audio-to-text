cbaa1279c3f7032a243d7af39a9e47b8eaf63af9
dr.burger <mr.krotkov.mm@mail.ru>
2023-06-05 12:59:29 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import whisper
+
+
+def speech_recognition(model='base'):
+    """ Recognizes speech from an audio file passed to it """
+
+    speech_model = whisper.load_model(model)
+    result = speech_model.transcribe('miyagi_-_samuray.mp3', fp16=False)
+
+    # write the recognized speech to a text file
+    with open(f'transcription_{model}.txt', 'w') as file:
+        file.write(result['text'])
+
+
+def main():
+    # models of whisper module (read Doc.)
+    models = {1: 'tiny', 2: 'base', 3: 'small', 4: 'medium', 5: 'large'}
+
+    for k, v in models.items():
+        print(f'{k}: {v}')
+
+    model = int(input('Choose a model by writing a number from 1 to 5: '))
+
+    if model not in models.keys():
+        raise KeyError(f'Model {model} is not in the list!')
+
+    print('The transcription process has started, please wait...\n')
+    speech_recognition(model=models[model])
+    print('Готово!✅')
+
+
+if __name__ == '__main__':
+    main()
